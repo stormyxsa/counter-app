291d92107ed13be3970dcf393127e5d9e3900340
Daniel Chinedum <chinedum.d6027@gmail.com>
2025-08-18 11:13:20 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import {useState, useEffect } from 'react';
+import './App.css';
+
+
+
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    setCounter(100);
+  }, []);
+  return (
+    <div className="App">
+    <button onClick={() => setCounter((prevCount)=> prevCount - 1)}>-</button>
+    <h1>{counter}</h1>
+     <button onClick={() => setCounter((prevCount)=> prevCount + 1)}>+</button>
+
+    </div>
+  );
+}
+
+export default App;
